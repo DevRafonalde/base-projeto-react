4822e6075ae4bd2a461314d5e948f17417a6c284
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-15 10:43:55 -0300

    Redux Saga

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,24 +2,24 @@ import React from "react";
 import {useDispatch} from "react-redux";
 import {Container} from "../../styles/GlobalStyles";
 import {Title, Paragrafo} from "./styled";
-import axios from "../../services/axios";
+// import axios from "../../services/axios";
 import * as exampleActions from "../../store/modules/example/actions";
 
 export default function Login() {
     const dispatch = useDispatch();
 
-    React.useEffect(() => {
-        async function getData() {
-            const response = await axios.get("/alunos");
-            const {data} = response;
-            console.log(data);
-        }
-        getData();
-    }, []);
+    // React.useEffect(() => {
+    //     async function getData() {
+    //         const response = await axios.get("/alunos");
+    //         const {data} = response;
+    //         console.log(data);
+    //     }
+    //     getData();
+    // }, []);
 
     function handleClick(e) {
         e.preventDefault();
-        dispatch(exampleActions.clicaBotao());
+        dispatch(exampleActions.clicaBotaoRequest());
     }
 
     return (
--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,3 +1,6 @@
+import {toast} from "react-toastify";
+import * as types from "../types";
+
 const initialState = {
     botaoClicado: false,
 };
@@ -5,11 +8,22 @@ const initialState = {
 // eslint-disable-next-line default-param-last
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case "BOTAO_CLICADO": {
+        case types.BOTAO_CLICADO_REQUEST: {
+            console.log("Estou fazendo a requisição");
+            toast.info("Estou fazendo a requisição");
+            return state;
+        }
+        case types.BOTAO_CLICADO_SUCCESS: {
             const newState = {...state};
             newState.botaoClicado = !newState.botaoClicado;
-            // console.log(newState);
+            console.log("Opa, sucesso!");
+            toast.success("Opa, sucesso!");
             return newState;
+        }
+        case types.BOTAO_CLICADO_FAILURE: {
+            toast.error("Deu erro 😭😭");
+            console.log("Deu erro 😭😭");
+            return state;
         }
         default: {
             return state;
